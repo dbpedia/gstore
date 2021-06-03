cca6e71602115f431a910b8746df56b5b29dbbf4
Kirill Yankov <yankow@infai.org>
2021-06-03 16:03:40 +0300

    Tractate test

--- src/test/scala/org/dbpedia/databus/DatabusScalatraTest.scala
+++ src/test/scala/org/dbpedia/databus/DatabusScalatraTest.scala
@@ -1,8 +1,11 @@
 package org.dbpedia.databus
 
 
+import java.io.ByteArrayInputStream
 import java.nio.file.{Files, Paths}
 
+import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.riot.{Lang, RDFDataMgr}
 import org.dbpedia.databus.ApiImpl.Config
 import org.dbpedia.databus.swagger.DatabusSwagger
 import org.dbpedia.databus.swagger.api.DefaultApi
@@ -55,6 +58,28 @@ class DatabusScalatraTest extends ScalatraFlatSpec {
     post("/databus/shacl/validate", Map.empty, Map("shacl" -> shacl, "graph" -> err)){
       //todo change to 400 or 200 but with error
       status should equal(500)
+    }
+  }
+
+  "Api" should "generate tractate" in {
+    val fl = "version_wrong.jsonld"
+    val version = Files.readAllBytes(Paths.get(getClass.getClassLoader.getResource(fl).getFile))
+
+    post("/databus/dataid/tractate", version){
+      status should equal(200)
+      body should equal("""Databus Tractate V1
+                          |https://webid.dbpedia.org/webid.ttl#this
+                          |http://databus.dbpedia.org/kuckuck/nest/eier/2020.10.10
+                          |http://purl.oclc.org/NET/rdflicense/cc-0
+                          |2020-12-06T00:00:00Z
+                          |1be509fb64371dcf5fc7df334964753da4f2d33ba2d86b8e10150dbf64beef27
+                          |""".stripMargin)
+
+      val model = ModelFactory.createDefaultModel()
+      val dataStream = new ByteArrayInputStream(version)
+      RDFDataMgr.read(model, dataStream, Lang.JSONLD)
+      val tr = Tractate.extract(model.getGraph, TractateV1.Version)
+      body should equal(tr.get.stringForSigning)
     }
   }
 
