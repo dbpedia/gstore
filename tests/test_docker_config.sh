bb6c2e0c46f8778f56f38bac66649feec65bfacb
kurzum <kurzum@github.com>
2021-11-24 09:19:39 +0100

    intial test scripts

--- tests/test_docker_config.sh
+++ tests/test_docker_config.sh
@@ -5,11 +5,28 @@ source ./custom-assert.sh
 
 # Check general availability
 code=$(get_return_code localhost:3002)
-assert_eq "$code" "200" "G-Store reachable"
+assert_eq "$code" "200" "$code G-Store reachable"
+code=$(get_return_code localhost:3002/)
+assert_eq "$code" "200" "$code G-Store reachable"
+code=$(get_return_code localhost:3002/shacl)
+assert_eq "$code" "200" "$code G-Store reachable"
+
+# GIT File Viewer
+code=$(get_return_code localhost:3002/git)
+assert_eq "$code" "200" "/git/ viewer reachable"
+code=$(get_return_code localhost:3002/git/)
+assert_eq "$code" "200" "/git/ viewer reachable"
+
 
 # Check virtuoso availability
 code=$(get_return_code "localhost:3002/sparql?default-graph-uri=&query=ASK+%7B%3Fs+%3Fp+%3Fo%7D&format=text%2Fhtml" )
-assert_eq "$code" "200" "Virtuoso reachable"
+assert_eq "$code" "200" "Virtuoso SPARQL reachable via Jetty proxy"
+
+code=$(get_return_code "localhost:3003/sparql?default-graph-uri=&query=ASK+%7B%3Fs+%3Fp+%3Fo%7D&format=text%2Fhtml" )
+assert_eq "$code" "200" "Virtuoso SPARQL reachable via port 3003"
+
+
+
 
 #result=$(curl https://databus.dbpedia.org/system/api/search?query=data -s)
 #assert_not_eq "$result" "{\"docs\":[]}" "Nothing found for query 'data'"
