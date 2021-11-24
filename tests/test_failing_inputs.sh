8703337c6051bbd0b5c2bcde6e457d7a53d23eaa
Jan Forberg <janforberg@pop-os.localdomain>
2021-11-24 10:45:21 +0100

    added helper function

--- tests/test_failing_inputs.sh
+++ tests/test_failing_inputs.sh
@@ -1,5 +1,10 @@
 source ./custom-assert.sh
 
+get_post_return_code() {
+  >&2 printf "Test ${BLUE}%s${NORMAL}\n" "curl -f -Li -H "Content-Type:application/json+ld" -d $2 $1 -o /dev/null -w '%{http_code}\n' -s"
+  echo $(curl -f -Li -H "Content-Type:application/json+ld" -d $2 $1 -o /dev/null -w '%{http_code}\n' -s)
+}
+
 # Writing to the gstore for failing input (22.11.2021): virtuoso.jdbc4.VirtuosoException: SQ200: No column NaN.
-code=$(get_return_code "-d @data/no-column-nan.jsonld -H \"Content-Type:application/json+ld\" \"http://localhost:3002/file/save?repo=testing&path=test/no-column-nan.jsonld\"")
+code=$(get_post_return_code "http://localhost:3002/file/save?repo=testing&path=test/no-column-nan.jsonld" @data/no-column-nan.jsonld)
 assert_eq "$code" "200" "Writing to G-Store successful"
