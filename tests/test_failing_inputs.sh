5b1b641679f55d2d7a8b0c66a53a7c0c6258e5d9
Jan Forberg <janforberg@pop-os.localdomain>
2021-11-24 10:46:09 +0100

    changed file path

--- tests/test_failing_inputs.sh
+++ tests/test_failing_inputs.sh
@@ -6,5 +6,5 @@ get_post_return_code() {
 }
 
 # Writing to the gstore for failing input (22.11.2021): virtuoso.jdbc4.VirtuosoException: SQ200: No column NaN.
-code=$(get_post_return_code "http://localhost:3002/file/save?repo=testing&path=test/no-column-nan.jsonld" @data/no-column-nan.jsonld)
+code=$(get_post_return_code "http://localhost:3002/file/save?repo=testing&path=test/no-column-nan.jsonld" @data/failing/no-column-nan.jsonld)
 assert_eq "$code" "200" "Writing to G-Store successful"
