1c88ab25fc2dfaa8ab0cd6fc617d44ac10a0a0b5
Jan <marvin@pop-os.localdomain>
2023-08-16 10:27:48 +0200

    enabled caching of context on localhosts

--- src/main/scala/org/dbpedia/databus/CachingJsonldContext.scala
+++ src/main/scala/org/dbpedia/databus/CachingJsonldContext.scala
@@ -1,6 +1,7 @@
 package org.dbpedia.databus
 
 import java.util.concurrent.ConcurrentHashMap
+import java.net.URL
 
 import com.github.jsonldjava.core.{Context, JsonLdOptions}
 import org.dbpedia.databus.CachingJsonldContext.ApproxSizeStringKeyCache
@@ -11,21 +12,45 @@ class CachingJsonldContext(sizeLimit: Int, opts: JsonLdOptions) extends Context(
 
   private val cache = new ApproxSizeStringKeyCache[Context](sizeLimit)
 
+  private def isLocalhostURL(url: String): Boolean = {
+    val localhostPatterns = List("http://localhost", "localhost", "127.0.0.1", "::1")
+    localhostPatterns.exists(pattern => url.startsWith(pattern))
+  }
+
+  // Parse with fallback for localhost URLs. This can help for development when working with
+  // context URLs on the localhost. Should a localhost request fail, the parser will
+  // attempt to call the URL defined in GSTORE_LOCALHOST_CONTEXT_FALLBACK_URL instead.
+  // (defaults to the docker host IP)
+  private def tryParseWithFallback(s: String): Context = {
+    try {
+      val re = super.parse(s)
+      cache.put(s, re)
+      re
+    } catch {
+      case _: Exception if isLocalhostURL(s) =>
+        val dockerHostURL = sys.env.getOrElse("GSTORE_LOCALHOST_CONTEXT_FALLBACK_URL", "http://172.17.0.1")
+        val originalURL = new URL(s)
+        val portPart = if (originalURL.getPort != -1) ":" + originalURL.getPort else ""
+        val dockerHostContextURL = new URL(dockerHostURL + portPart + originalURL.getPath)
+        cache.get(dockerHostContextURL.toString)
+          .map(c => super.parse(c))
+          .getOrElse({
+            val re = super.parse(dockerHostContextURL.toString)
+            cache.put(s, re)
+            re
+          })
+      case _: Exception => super.parse(s)
+    }
+  }
+
   override def parse(ctx: Object): Context =
     ctx match {
       case s: String =>
         cache.get(s)
           .map(c => super.parse(c))
-          .getOrElse({
-            val re = super.parse(ctx)
-            cache.put(s, re)
-            re
-          })
+          .getOrElse(tryParseWithFallback(s))
       case _ => super.parse(ctx)
     }
-
-
-
 }
 
 object CachingJsonldContext {
